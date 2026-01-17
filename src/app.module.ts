83cd5a29b5cd16631be11e20727bdca306375c2c
Ganesh Adsul <ganeshadsul76@gmail.com>
2026-01-18 00:07:50 +0530

    Database connection done.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          uri: configService.get<string>('MONGO_URI'),
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger('Database');
+  constructor(@InjectConnection() private connection: Connection) {}
+
+  onModuleInit() {
+    if (this.connection.readyState === 1) {
+      this.logSuccess();
+    } else {
+      this.connection.on('connected', () => this.logSuccess());
+    }
+  }
+
+  private logSuccess() {
+    this.logger.log('✅ MongoDB connection successful');
+    this.logger.log(`📁 Database Name: ${this.connection.name}`);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
