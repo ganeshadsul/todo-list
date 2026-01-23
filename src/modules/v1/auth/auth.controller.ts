1f8bfd32c6a7d70ec889b5eff2eb1acfade2758d
Ganesh Adsul <ganeshadsul76@gmail.com>
2026-01-23 20:35:08 +0530

    User auth signup route created.

--- src/modules/v1/auth/auth.controller.ts
+++ src/modules/v1/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { LoginDto } from './dto/login.dto';
 import { AuthService } from './auth.service';
 import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
 import { AUTH_MESSAGES } from 'src/common/constants/message.constant';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Controller({
   path: 'auth',
@@ -16,5 +17,12 @@ export class AuthController {
   @ResponseMessage(AUTH_MESSAGES.SUCCES)
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
+  }
+
+  @Post('/signup')
+  @HttpCode(HttpStatus.CREATED)
+  @ResponseMessage('Signup complete.')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.signup(createUserDto);
   }
 }
--- src/modules/v1/auth/auth.service.ts
+++ src/modules/v1/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { User as UserModel } from './../users/schemas/user.schema';
 import { Model } from 'mongoose';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from '../users/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -27,5 +28,23 @@ export class AuthService {
     return {
       accessToken: this.jwtService.sign(payload),
     };
+  }
+
+  async signup(createUserDto: CreateUserDto) {
+    const { firstName, lastName, email, password, passwordConfirm } =
+      createUserDto;
+    const result = await this.User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+      passwordConfirm,
+    });
+    const user = result.toObject();
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: _, __v, _id, ...filteredUser } = user;
+
+    return filteredUser;
   }
 }
